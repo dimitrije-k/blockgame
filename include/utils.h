b39e6c9e7f5438aa5ba548efe16a19d3f03d8035
menucoder <tubulargmd@gmail.com>
2022-01-28 10:33:29 +0100

    added 'utils.h'

---
+++ include/utils.h
@@ -0,0 +1,19 @@
+#ifndef _utils_h
+#define _utils_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short u16;
+typedef signed short u16;
+typedef unsigned int u32;
+typedef signed int u32;
+
+typedef float f32;
+typedef double f64;
+
+#endif
